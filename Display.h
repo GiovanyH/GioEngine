6f6b0346b5ad7e7ee2a30e9f4c93265a311f5914
GiovanyH <56926698+GiovanyH@users.noreply.github.com>
2020-12-25 09:24:24 -0300

    Add files via upload

--- Display.h
+++ Display.h
@@ -1,8 +1,7 @@
 #include "System.h"
 
-class Display
+struct Display
 {
-public:
 	static void init(std::string name, unsigned int windowWidth, unsigned int windowHeight); // void for now
 
 	static void renderClear();
