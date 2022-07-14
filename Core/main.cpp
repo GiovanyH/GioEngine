9c2008c3f27f3b5c02be893aa594f28c587178a0
GiovanyH <henriquegiovany677@gmail.com>
2022-07-14 20:53:36 -0300

    Not exactly what I wanted because apparently you can't use them with ImGui::Begin

--- Core/main.cpp
+++ Core/main.cpp
@@ -22,9 +22,6 @@
 
 #include "imfilebrowser.h"
 #include <math.h>
-# include "imgui_internal.h"
-# include "imgui_node_editor.h"
-# include "application.h"
 
 // Better way of doing this later
 bool show_text_editor = false;
@@ -300,55 +297,6 @@ void canvas_drawing(bool *p_open) {
 	ImGui::End();
 }
 
-namespace ed = ax::NodeEditor;
-
-static ed::EditorContext* g_Context = nullptr;
-
-const char* Application_GetName()
-{
-    return "Simple";
-}
-
-void Application_Initialize()
-{
-    ed::Config config;
-    config.SettingsFile = "Simple.json";
-    g_Context = ed::CreateEditor(&config);
-}
-
-void Application_Finalize()
-{
-    ed::DestroyEditor(g_Context);
-}
-
-void Application_Frame()
-{
-    auto& io = ImGui::GetIO();
-
-    ImGui::Text("FPS: %.2f (%.2gms)", io.Framerate, io.Framerate ? 1000.0f / io.Framerate : 0.0f);
-
-    ImGui::Separator();
-
-    ed::SetCurrentEditor(g_Context);
-    ed::Begin("My Editor", ImVec2(0.0, 0.0f));
-    int uniqueId = 1;
-    // Start drawing nodes.
-    ed::BeginNode(uniqueId++);
-        ImGui::Text("Node A");
-        ed::BeginPin(uniqueId++, ed::PinKind::Input);
-            ImGui::Text("-> In");
-        ed::EndPin();
-        ImGui::SameLine();
-        ed::BeginPin(uniqueId++, ed::PinKind::Output);
-            ImGui::Text("Out ->");
-        ed::EndPin();
-    ed::EndNode();
-    ed::End();
-    ed::SetCurrentEditor(nullptr);
-
-	//ImGui::ShowMetricsWindow();
-}
-
 int main(int argc, char** argv) {
 	glfwSetErrorCallback(glfw_error_callback);
 	if (!glfwInit()) return 1;
@@ -408,8 +356,6 @@ int main(int argc, char** argv) {
 
 	std::string fileToEdit;
 
-	Application_Initialize();
-
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		
@@ -435,8 +381,6 @@ int main(int argc, char** argv) {
 		}
 
 		SimpleOverlay();
-
-		Application_Frame();
 
 		if(show_text_editor && std::filesystem::exists(get_current_project_dir())) {
 			fileToEdit = (get_current_project_dir() + "/src/main.rs").c_str();
@@ -478,7 +422,6 @@ int main(int argc, char** argv) {
 		glfwSwapBuffers(window);
 	}
 
-	Application_Finalize();
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
 	ImGui::DestroyContext();
