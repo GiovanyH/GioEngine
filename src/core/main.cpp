73ab3997ba8b5f5134319390e5c86a3e366aadc3
GiovanyH <henriquegiovany677@gmail.com>
2022-07-23 09:18:14 -0300

    It works ...Badly

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -20,6 +20,169 @@
 #include "editor.h"
 #include <math.h>
 
+#include <functional>
+#include <filesystem>
+#ifdef ZEP_CONSOLE
+#include <zep\imgui\console_imgui.h>
+#endif
+namespace fs = std::filesystem;
+
+#define APP_ROOT "/home/giovany/Development/GioEngine/src/editor/zep"
+using namespace Zep;
+
+using cmdFunc = std::function<void(const std::vector<std::string>&)>; 
+class ZepCmd : public ZepExCommand
+{
+public:
+    ZepCmd(ZepEditor& editor, const std::string name, cmdFunc fn)
+        : ZepExCommand(editor)
+        , m_name(name)
+        , m_func(fn)
+    {
+    }
+
+    virtual void Run(const std::vector<std::string>& args) override
+    {
+        m_func(args);
+    }
+
+    virtual const char* ExCommandName() const override
+    {
+        return m_name.c_str();
+    }
+
+private:
+    std::string m_name;
+    cmdFunc m_func;
+};
+
+struct ZepWrapper : public Zep::IZepComponent
+{
+    ZepWrapper(const fs::path& root_path, const Zep::NVec2f& pixelScale, std::function<void(std::shared_ptr<Zep::ZepMessage>)> fnCommandCB)
+        : zepEditor(Zep::ZepPath(root_path.string()), pixelScale)
+        , Callback(fnCommandCB)
+    {
+        zepEditor.RegisterCallback(this);
+
+    }
+
+    virtual Zep::ZepEditor& GetEditor() const override
+    {
+        return (Zep::ZepEditor&)zepEditor;
+    }
+
+    virtual void Notify(std::shared_ptr<Zep::ZepMessage> message) override
+    {
+        Callback(message);
+
+        return;
+    }
+
+    virtual void HandleInput()
+    {
+        zepEditor.HandleInput();
+    }
+
+    Zep::ZepEditor_ImGui zepEditor;
+    std::function<void(std::shared_ptr<Zep::ZepMessage>)> Callback;
+};
+
+#ifdef ZEP_CONSOLE
+std::shared_ptr<ImGui::ZepConsole> spZep;
+#else
+std::shared_ptr<ZepWrapper> spZep;
+#endif
+
+void zep_init(const Zep::NVec2f& pixelScale)
+{
+#ifdef ZEP_CONSOLE
+    spZep = std::make_shared<ImGui::ZepConsole>(Zep::ZepPath(APP_ROOT));
+#else
+    // Initialize the editor and watch for changes
+    spZep = std::make_shared<ZepWrapper>(APP_ROOT, Zep::NVec2f(pixelScale.x, pixelScale.y), [](std::shared_ptr<ZepMessage> spMessage) -> void {
+    });
+#endif
+
+    auto& display = spZep->GetEditor().GetDisplay();
+    auto pImFont = ImGui::GetIO().Fonts[0].Fonts[0];
+    auto pixelHeight = pImFont->FontSize;
+    display.SetFont(ZepTextType::UI, std::make_shared<ZepFont_ImGui>(display, pImFont, int(pixelHeight)));
+    display.SetFont(ZepTextType::Text, std::make_shared<ZepFont_ImGui>(display, pImFont, int(pixelHeight)));
+    display.SetFont(ZepTextType::Heading1, std::make_shared<ZepFont_ImGui>(display, pImFont, int(pixelHeight * 1.5)));
+    display.SetFont(ZepTextType::Heading2, std::make_shared<ZepFont_ImGui>(display, pImFont, int(pixelHeight * 1.25)));
+    display.SetFont(ZepTextType::Heading3, std::make_shared<ZepFont_ImGui>(display, pImFont, int(pixelHeight * 1.125)));
+
+}
+
+void zep_update()
+{
+    if (spZep)
+    {
+        spZep->GetEditor().RefreshRequired();
+    }
+}
+
+void zep_destroy()
+{
+    spZep.reset();
+}
+
+ZepEditor& zep_get_editor()
+{
+    return spZep->GetEditor();
+}
+
+void zep_load(const Zep::ZepPath& file)
+{
+#ifndef ZEP_CONSOLE
+    auto pBuffer = zep_get_editor().InitWithFileOrDir(file);
+#endif
+}
+
+void zep_show(const Zep::NVec2i& displaySize)
+{
+    bool show = true;
+#ifdef ZEP_CONSOLE
+    spZep->Draw("Console", &show, ImVec4(0, 0, 500, 400), true);
+    spZep->AddLog("Hello!");
+#else
+    ImGui::SetNextWindowSize(ImVec2(displaySize.x, displaySize.y), ImGuiCond_FirstUseEver);
+    if (!ImGui::Begin("Zep", &show, ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_MenuBar))
+    {
+        ImGui::End();
+        return;
+    }
+
+    auto min = ImGui::GetCursorScreenPos();
+    auto max = ImGui::GetContentRegionAvail();
+    if (max.x <= 0)
+        max.x = 1;
+    if (max.y <= 0)
+        max.y = 1;
+    ImGui::InvisibleButton("ZepContainer", max);
+
+    // Fill the window
+    max.x = min.x + max.x;
+    max.y = min.y + max.y;
+
+    spZep->zepEditor.SetDisplayRegion(Zep::NVec2f(min.x, min.y), Zep::NVec2f(max.x, max.y));
+    spZep->zepEditor.Display();
+    bool zep_focused = ImGui::IsWindowFocused();
+    if (zep_focused)
+    {
+        spZep->zepEditor.HandleInput();
+    }
+
+    // TODO: A Better solution for this; I think the audio graph is creating a new window and stealing focus
+    static int focus_count = 0;
+    if (focus_count++ < 2)
+    {
+        ImGui::SetWindowFocus();
+    }
+    ImGui::End();
+#endif
+}
+
 using namespace ghc;
 
 bool show_text_editor = true;
@@ -286,10 +449,12 @@ int main(int argc, char** argv) {
 	ImFont * regular = io.Fonts->AddFontFromFileTTF("../style/fonts/Ubuntu-Bold.ttf", 17);
 	ImFont * code = io.Fonts->AddFontFromFileTTF("../style/fonts/SpaceMono-Bold.ttf", 19); 
 
-	//std::string fileToEdit;
+	std::string fileToEdit;
 
 	ImVec2 penis;
 	penis.x = 0; penis.y = 200;
+
+	bool show_z = true;
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -325,20 +490,32 @@ int main(int argc, char** argv) {
 
 		ImGui::PushFont(code);
 
-		if(!show_text_editor && filesystem::exists(get_current_project_dir())) {
+		if(filesystem::exists(get_current_project_dir()) && show_z && show_text_editor) {
+			std::cout << "Hello" << std::endl;
             zep_init(Zep::NVec2f(1.0f, 1.0f));
-			//fileToEdit = (get_current_project_dir() + "/src/main.rs").c_str();
-            zep_load(Zep::ZepPath(".") / "src" / "main.cpp");
+			fileToEdit = (get_current_project_dir() + "/src/main.rs").c_str();
+            zep_load(Zep::ZepPath("/home/giovany/Development/GioEngine/src/editor/zep") / "src" / "main.cpp");
 
-			show_text_editor = true;
+			show_z = false;
 		}
 
-		// Required for CTRL+P and flashing cursor.
-        zep_update();
+		if(filesystem::exists(get_current_project_dir()) && show_text_editor) {
+			std::cout << "Hello" << std::endl;
+			// Required for CTRL+P and flashing cursor.
+			zep_update();
 
-        // Just show it
-        static Zep::NVec2i size = Zep::NVec2i(640, 480);
-        zep_show(size);
+
+			std::cout << "Hello" << std::endl;
+
+			// Just show it
+			static Zep::NVec2i size = Zep::NVec2i(640, 480);
+
+
+			std::cout << "Hello" << std::endl;
+			zep_show(size);
+
+			std::cout << "Hello" << std::endl;
+		}
 
 		//CanvasBegin("Editor", nullptr);
 
